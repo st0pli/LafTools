bc6f2e939c2d038e32623e24050f211740907c94
LafTools Team - FX <work7z@outlook.com>
2024-04-02 22:44:22 +0800

    Update run-docker.sh script to remove unnecessary parameter

--- modules/server2/docker/run-docker.sh
+++ modules/server2/docker/run-docker.sh
@@ -3,18 +3,12 @@
 
 source /home/appuser/Server2App-dist/env.sh
 
-crtVersion=$1
-containerName=$2
-if [ -z $crtVersion ]; then
-    echo "[E] crtVersion is required."
-    exit 1
-fi
-
-echo "[I] crtVersion: $crtVersion"
+containerName=$1
+crtVersion=$2
 
 docker ps -a | grep $containerName | awk '{print $1}' | xargs -I {} docker stop {}
-docker ps -a | grep $containerName | awk '{print $1}' | xargs -I {} docker rm {}
-docker run -e DB_USER=$DB_USER -e DB_PASSWORD=$DB_PASSWORD -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_DATABASE=$DB_DATABASE -e TYKEY=$TYKEY --name $containerName -d -p 0.0.0.0:2016:2016 -v /home/appuser/Server2App-dist/logs:/opt/logs:rw localbuild/server2-linux-x64:devops 
+# docker ps -a | grep $containerName | awk '{print $1}' | xargs -I {} docker rm {}
+docker run -e DB_USER=$DB_USER -e DB_PASSWORD=$DB_PASSWORD -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_DATABASE=$DB_DATABASE -e TYKEY=$TYKEY --name $containerName -d -p 0.0.0.0:2016:2016 -v /home/appuser/Server2App-dist/logs:/opt/logs:rw localbuild/server2-linux-x64:$crtVersion tail -f /dev/null 
 timeout 60 docker logs -f $containerName
 
 exit 0
