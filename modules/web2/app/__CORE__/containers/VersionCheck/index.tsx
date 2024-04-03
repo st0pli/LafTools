55641bb19903c62bbc3d1fa431c91ddb514908c8
LafTools Team - FX <work7z@outlook.com>
2024-04-04 00:57:10 +0800

    Add server mode check in VersionCheck component

--- modules/web2/app/__CORE__/containers/VersionCheck/index.tsx
+++ modules/web2/app/__CORE__/containers/VersionCheck/index.tsx
@@ -5,9 +5,12 @@ import { URL_RELEASE_GET_LATEST } from "@/app/[lang]/client/src/api/url"
 import _ from "lodash"
 import { useCallback, useEffect } from "react"
 import { getCurrentLang } from "../../utils/cTranslationUtils"
+import { IsCurrentServerMode, IsCurrentServerModeWithPromise } from "../../utils/systemUtils"
 
 export default () => {
-    let fn = useCallback(_.once(() => {
+    let fn = useCallback(_.once(async () => {
+        let serverMode = await IsCurrentServerModeWithPromise()
+        if (serverMode) return;
         sendAPIRequestInBE({
             lang: getCurrentLang()
         }, URL_RELEASE_GET_LATEST, {})
