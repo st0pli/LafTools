a538df542a5e254b4674e1e0806e28b6c803bae0
LafTools Team - FX <work7z@outlook.com>
2024-04-06 22:41:51 +0800

    Refactored run-docker.sh script to improve container management

--- modules/server2/docker/run-docker.sh
+++ modules/server2/docker/run-docker.sh
@@ -8,12 +8,33 @@ crtVersion=$2
 
 # TODO: health check before replacing container in case any error
 
-timeout 20 docker ps -a | grep $containerName | awk '{print $1}' | xargs -I {} docker stop {}
-docker ps -a | grep $containerName | awk '{print $1}' | xargs -I {} docker rm {}
+killCP(){
+    pContainerName=$1
+    pListenPort=$2
+    timeout 20 docker ps -a | grep $pContainerName | awk '{print $1}' | xargs -I {} docker stop {}
+    docker ps -a | grep $pContainerName | awk '{print $1}' | xargs -I {} docker rm {}
+}
+runCP(){
+    pContainerName=$1
+    pListenPort=$2
+    docker run -e DB_USER=$DB_USER -e DB_PASSWORD=$DB_PASSWORD -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_DATABASE=$DB_DATABASE -e TYKEY=$TYKEY --name $pContainerName -d -p 0.0.0.0:$pListenPort:2016 -v /home/appuser/Server2App-dist/logs:/opt/logs:rw localbuild/server2-linux-x64:$crtVersion
+}
+# test pre
+preContainerName=${containerName}preinst
+prePort=2019
+killCP $preContainerName $prePort
+runCP $preContainerName $prePort
+sleep 15
+curl 127.0.0.1:2016/v3/hello-world -I | grep "200 OK"
+if [ $? -ne 0 ]; then
+    echo "[PREINST FAILED] Failed to start container $preContainerName"
+    exit 1
+fi
 
-
-
-docker run -e DB_USER=$DB_USER -e DB_PASSWORD=$DB_PASSWORD -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_DATABASE=$DB_DATABASE -e TYKEY=$TYKEY --name $containerName -d -p 0.0.0.0:2016:2016 -v /home/appuser/Server2App-dist/logs:/opt/logs:rw localbuild/server2-linux-x64:$crtVersion
+# run current
+port=2016
+killCP $containerName $port
+runCP $containerName $port
 if [ $? -ne 0 ]; then
     echo "Failed to start container $containerName"
     exit 1
