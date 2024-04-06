a98a3e56284ec3f7d521119fa7b7ccb86b72bbc7
LafTools Team - FX <work7z@outlook.com>
2024-04-06 22:34:05 +0800

    Add health check before replacing container

--- modules/server2/docker/run-docker.sh
+++ modules/server2/docker/run-docker.sh
@@ -6,8 +6,13 @@ source /home/appuser/Server2App-dist/env.sh
 containerName=$1
 crtVersion=$2
 
+# TODO: health check before replacing container in case any error
+
 timeout 20 docker ps -a | grep $containerName | awk '{print $1}' | xargs -I {} docker stop {}
 docker ps -a | grep $containerName | awk '{print $1}' | xargs -I {} docker rm {}
+
+
+
 docker run -e DB_USER=$DB_USER -e DB_PASSWORD=$DB_PASSWORD -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_DATABASE=$DB_DATABASE -e TYKEY=$TYKEY --name $containerName -d -p 0.0.0.0:2016:2016 -v /home/appuser/Server2App-dist/logs:/opt/logs:rw localbuild/server2-linux-x64:$crtVersion
 if [ $? -ne 0 ]; then
     echo "Failed to start container $containerName"
