87bd52d106c9994ba64a5b7953945d3aae78a781
LafTools Team - FX <work7z@outlook.com>
2024-04-10 00:21:13 +0800

    Update Dockerfile and bootstrap.sh in server2 module

--- modules/meta/scripts/testflow.sh
+++ modules/meta/scripts/testflow.sh
@@ -3,4 +3,4 @@
 # on server
 docker stop test-normal-docker
 docker rm test-normal-docker
-docker run -e DEBUG_MODE=yes -e LAFREGION=CN -e APPLANG=zh_CN --name test-normal-docker -d -p 0.0.0.0:2066:39899 codegentoolbox/laftools-linux-x64:v2.1.98-beta
+docker run -e DEBUG_MODE=yes -e LAFREGION=CN -e APPLANG=zh_CN --name test-normal-docker -d -p 0.0.0.0:2066:39899 codegentoolbox/laftools-linux-x64:v2.2.11-beta
