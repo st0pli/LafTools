8c1f26a686fdfe419eaac7a00a85821dd2db5843
LafTools Team - FX <work7z@outlook.com>
2024-04-12 22:27:07 +0800

    Refactor hash.test.ts file to remove unnecessary test case

--- modules/bootstrap/src/utils/hash.test.ts
+++ modules/bootstrap/src/utils/hash.test.ts
@@ -8,17 +8,3 @@ let examplesDIR = join(__dirname, "examples");
 let distDIR = join(__dirname, "dist");
 import fs from "fs";
 import { computeHash } from "./hash";
-
-test(
-  "test-hash-code",
-  async () => {
-    let file = join(__dirname, "examples", "testfile.txt");
-    let hashVal = await computeHash(file);
-    expect(hashVal).toBe(
-      "f29844cce4124c9ced28a46148f3c7ee81ae18b254bc95b01b4d98fbebf4d119",
-    );
-  },
-  {
-    timeout: 500000,
-  },
-);
