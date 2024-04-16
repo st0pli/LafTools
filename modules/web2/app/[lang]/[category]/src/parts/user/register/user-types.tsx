94d9e77b8c0e7b908856011e1c76c52e4ddd122a
LafTools Team - FX <work7z@outlook.com>
2024-04-16 21:50:38 +0800

    Refactor user-types.tsx in web2 module

--- modules/web2/app/[lang]/[category]/src/parts/user/register/user-types.tsx
+++ modules/web2/app/[lang]/[category]/src/parts/user/register/user-types.tsx
@@ -2,16 +2,7 @@ import _ from "lodash";
 import { AuthInfoProps } from "@/app/[lang]/page";
 import dao from "@/app/__CORE__/dao";
 import { key_systemInfoGroup } from "./redis-types";
-import { User } from "@/app/__CORE__/dao/model";
 
-export let fn_get_user_avatar = (authInfoProps: AuthInfoProps) => {
-    let avatarPath = authInfoProps.authInfo?.user?.avatarPath
-    let userId = authInfoProps.authInfo.user?.id
-    if (_.isEmpty(avatarPath)) {
-        return '/avatar/dft-1.png'
-    }
-    return avatarPath
-}
 
 export type SystemInfoBody = {
     userCount: number,
@@ -20,9 +11,7 @@ export type SystemInfoBody = {
 }
 let launchBefore = false;
 export let fn_refresh_system_info_from_redis = async () => {
-    let daoRef = await dao()
-    let userCtn = await User.count()
-    await daoRef.redis.hSet(key_systemInfoGroup, 'userCount', userCtn)
+    //
 }
 export let fn_get_system_info_from_redis = async (): Promise<SystemInfoBody> => {
     if (!launchBefore) {
