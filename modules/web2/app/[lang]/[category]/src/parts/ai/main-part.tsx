394ea451744fa30b63315994a4ce637e7b115fa4
LafTools Team - FX <work7z@outlook.com>
2024-04-18 01:20:34 +0800

    Refactor AI main sidebar component and param state slice

--- modules/web2/app/[lang]/[category]/src/parts/ai/main-part.tsx
+++ modules/web2/app/[lang]/[category]/src/parts/ai/main-part.tsx
@@ -17,6 +17,8 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'use client'
+
 
 import React from 'react';
 import { border_clz, light_border_clz_all, tw } from '@/app/__CORE__/meta/styles';
