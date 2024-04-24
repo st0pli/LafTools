5e4f482ff4f0a2105f29aa55f4ef3d32a77c9415
LafTools Team - FX <work7z@outlook.com>
2024-04-24 23:24:07 +0800

    Update package-lock.json, package.json, App.tsx, tsconfig.json, vite.config.ts, main.tsx, and VerCompareUtils.test.tsx files in web2-spa module

--- modules/web2-spa/src/App.tsx
+++ modules/web2-spa/src/App.tsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 import './index.scss'
+import "./globals.css";
+// import Home from './[lang]/[category]/page';
+import CryptoApi from "crypto-api/src/crypto-api.mjs";
+import _ from 'lodash';
 
 function App() {
+  console.log(CryptoApi)
 
   return (
     <>
-      <div>this is main app</div>
+      <div className='   '>this is main app 20 </div>
+      <h1>
+        {
+          _.keys(CryptoApi)
+        }
+      </h1>
+      {/* <Home params={{
+        subCategory: '',
+        category: '',
+        id: ''
+      }} searchParams={{}} /> */}
     </>
   )
 }
--- modules/web2-spa/vite.config.ts
+++ modules/web2-spa/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import path from "path";
+import builtins from "rollup-plugin-node-builtins";
 
+const builtinsPlugin = {
+  ...builtins({ crypto: true }),
+  name: "rollup-plugin-node-builtins",
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), builtinsPlugin],
+
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  resolve: {
+    extensions: [".mjs", ".js", ".cjs", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+    alias: {
+      "@": path.join(__dirname, "src"),
+    },
+  },
+  optimizeDeps: {
+    exclude: ["crypto-api"],
+  },
+});
